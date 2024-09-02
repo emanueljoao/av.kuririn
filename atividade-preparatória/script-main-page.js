25596a0f6038ba4ad4e7540ebaefdde577406b93
João Emanuel <emanueljoaoss123@gmail.com>
2024-09-02 17:32:03 -0300

    começando a segunda section

--- atividade-preparatória/script-main-page.js
+++ atividade-preparatória/script-main-page.js
@@ -1,28 +1,54 @@
-// //SELETORES FUNÇÕES
-// const menu = document.querySelector('.menu')
-// const menuMain = document.querySelector('.conteudo-menu')
-// const menuMobile = document.querySelector('.menu-mobile .menu')
-// const header = document.querySelector('nav')
+//SELETORES FUNÇÕES
+const menu = document.querySelector('.menu')
+const menuMain = document.querySelector('.conteudo-menu')
+const menuMobile = document.querySelector('.menu-mobile .menu')
+const header = document.querySelector('nav')
 
-// // FUNÇÕES
-// function functionMenu() {
-//     menuMain.classList.toggle('active')
-//     menu.classList.toggle('active')
-//     menuMobile.classList.toggle('active')
-//     header.classList.toggle('active')
-// }
+// FUNÇÕES
+function functionMenu() {
+    menuMain.classList.toggle('active')
+    menu.classList.toggle('active')
+    menuMobile.classList.toggle('active')
+    header.classList.toggle('active')
+}
 
 
-// // EVENTOS
-// menu.addEventListener('click', functionMenu)
-// menuMobile.addEventListener('click', functionMenu)
-
+// EVENTOS
+menu.addEventListener('click', functionMenu)
+menuMobile.addEventListener('click', functionMenu)
+// SWIPER - SECTION ULTIMOS LOTES
 var swiper = new Swiper(".mySwiper", {
     navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
+        nextEl: ".swiper-button-next-custom",
+        prevEl: ".swiper-button-prev-custom",
       },
     pagination: {
-        el: ".swiper-pagination",
+        el: ".swiper-pagination-custom",
       },
+});
+
+// SWIPER - SECTION NOVIDADES DO BLOG
+
+var swiperBlog = new Swiper(".mySwiperBlog", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: ".swiper-button-next-blog",
+    prevEl: ".swiper-button-prev-blog",
+  },
+  breakpoints: {
+    576: {
+      slidesPerView: 2,
+    }, 
+}});
+
+// SWIPER - SECTION PRÓXIMOS LANCAMENTOS 
+
+var swiperNextLance = new Swiper(".mySwiperLancamentos", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: ".swiper-button-next-prox-lance",
+    prevEl: ".swiper-button-prev-prox-lance",
+  }, 
 });

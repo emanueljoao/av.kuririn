7edde3e7fcc176cf5451b735f60ac05b7d85ae4f
João Emanuel <emanueljoaoss123@gmail.com>
2024-09-24 17:11:08 -0300

    terminei 2 sections

--- avaliação-kuririn/javascripts/script-swiper.js
+++ avaliação-kuririn/javascripts/script-swiper.js
@@ -1,4 +1,4 @@
-// SWIPER - SECTION ULTIMOS LOTES
+// SWIPER - SECTION BANNER MOBILIDADE
 var swiper = new Swiper(".mySwiperMobilidade", {
     navigation: {
         nextEl: ".swiper-button-next-custom",
@@ -9,3 +9,34 @@ var swiper = new Swiper(".mySwiperMobilidade", {
         clickable: true,
       },
 });
+
+
+// SWIPER - SECTION PRODUTOS E SERVIÇOS
+var swiperProdutosServicos = new Swiper(".mySwiperProdutosServicos", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  navigation: {
+      nextEl: ".swiper-button-next-custom",
+      prevEl: ".swiper-button-prev-custom",
+    },
+  breakpoints: {
+
+    768: {
+      slidesPerView: 2,
+    },
+
+    992: {
+      slidesPerView:  2.5,
+    },
+  }
+});
+
+
+
+
+
+
+
+
+
+

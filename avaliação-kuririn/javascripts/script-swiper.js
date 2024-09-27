fe0af410dfba540dbc479a28cd4613b522ac425a
João Emanuel <emanueljoaoss123@gmail.com>
2024-09-27 17:13:54 -0300

    terminei as novidades do blog

--- avaliação-kuririn/javascripts/script-swiper.js
+++ avaliação-kuririn/javascripts/script-swiper.js
@@ -33,14 +33,48 @@ var swiperProdutosServicos = new Swiper(".mySwiperProdutosServicos", {
 
 // SWIPER - SECTION SEGMENTOS E SETORES
 var swiperSegmentosSetores = new Swiper(".myswiperSegmentosSetores", {
-  slidesPerView: 3,
+  slidesPerView: 1,
   navigation: {
-      nextEl:".swiper-button-next",
-      prevEl:".swiper-button-prev",
+      nextEl:".swiper-button-next-custom",
+      prevEl:".swiper-button-prev-custom",
   },
+  breakpoints: {
+    576: {
+      slidesPerView: 2,
+    },
+    992: {
+      slidesPerView:3,
+    },
+  }
 });
 
-
+// SWIPER - SECTION NOVIDADES DO BLOG
+var swiperNovidadesDoBlog = new Swiper(".myswiperNovidadesBlog", {
+  slidesPerView: 1,
+  spaceBetween: 15,
+  navigation: {
+    nextEl:".swiper-button-next-custom",
+    prevEl:".swiper-button-prev-custom",
+  },
+  breakpoints: {
+    576: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    992: {
+      slidesPerView: 'auto',
+      spaceBetween: 30,
+    },
+    1200: {
+      slidesPerView:3,
+      spaceBetween: 30,
+    },
+  }
+});
 
 
 

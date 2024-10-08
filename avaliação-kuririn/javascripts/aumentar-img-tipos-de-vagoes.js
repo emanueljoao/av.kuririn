8c906eb39c17238c6b215b0f54c7f54ee4839a48
João Emanuel <emanueljoaoss123@gmail.com>
2024-10-08 17:05:05 -0300

    praticamente terminei a page de produtos e serviços

---
+++ avaliação-kuririn/javascripts/aumentar-img-tipos-de-vagoes.js
@@ -0,0 +1,8 @@
+// var swiperTipoDeVagao = new Swiper(".myswiperTiposDeVagoes", {
+//     slidesPerView: "auto",
+//     spaceBetween: 30,
+//     navigation: {
+//         nextEl:".swiper-button-next-custom",
+//         prevEl:".swiper-button-prev-custom",
+//     },
+// });
